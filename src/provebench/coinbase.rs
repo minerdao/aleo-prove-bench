db6bb024f675017093b366249e9c3924229a092c
jim luo <zmule@163.com>
2022-10-21 12:15:11 +0800

    fix: branch to feat/new-posw-prover

--- src/provebench/coinbase.rs
+++ src/provebench/coinbase.rs
@@ -4,15 +4,15 @@ use rand::{thread_rng, RngCore};
 use snarkvm::{
     algorithms::polycommit::kzg10::UniversalParams,
     curves::bls12_377::Bls12_377,
-    prelude::{Address, CanonicalDeserialize, CoinbaseProvingKey, CoinbasePuzzle, EpochChallenge, PrivateKey, PuzzleConfig, Testnet3},
+    prelude::{Address, CanonicalDeserialize, CoinbasePuzzle, EpochChallenge, PrivateKey, PuzzleConfig, Testnet3},
     utilities::serialize::*,
 };
 use std::{fs::File, io::Read, time::Duration};
 
 type CoinbasePuzzleInst = CoinbasePuzzle<Testnet3>;
-type ProvingKeyInst = CoinbaseProvingKey<Testnet3>;
+// type ProvingKeyInst = CoinbaseProvingKey<Testnet3>;
 
-fn setup_provekey(degree: u32) -> (ProvingKeyInst, u32) {
+fn setup_prover(degree: u32) -> (CoinbasePuzzleInst, u32) {
     let mut file = File::open("./universal.srs").expect("need universal20.srs file");
     let mut srs = Vec::new();
     file.read_to_end(&mut srs).expect("need to read the whole file");
@@ -24,20 +24,20 @@ fn setup_provekey(degree: u32) -> (ProvingKeyInst, u32) {
     print_title_info("Waiting", "Prove Setup, trim srs to prove key");
 
     let config = PuzzleConfig { degree: 2_u32.pow(degree) };
-    let provekey = CoinbasePuzzleInst::trim(&universal_srs, config).unwrap().0;
+    let prover = CoinbasePuzzleInst::trim(&universal_srs, config).unwrap();
 
-    (provekey, config.degree)
+    (prover, config.degree)
 }
 
-pub fn prove(pk: Box<ProvingKeyInst>, degree: u32, min_elapse: u64) {
+pub fn prove_by_degree(prover: Box<CoinbasePuzzleInst>, degree: u32, min_elapse: u64) {
     let duration = Duration::from_secs(min_elapse * 60);
     let result = benchmarking::bench_function_with_duration(duration, move |b| {
         b.measure(|| {
             let rng = &mut thread_rng();
-            let challenge: EpochChallenge<Testnet3> = EpochChallenge::new(rng.next_u64(), Default::default(), degree).unwrap();
+            let challenge: EpochChallenge<Testnet3> = EpochChallenge::new(rng.next_u32(), Default::default(), degree).unwrap();
             let address = Address::try_from(PrivateKey::new(rng).unwrap()).unwrap();
             let nonce = rng.next_u64();
-            CoinbasePuzzleInst::prove(&pk, &challenge, &address, nonce).unwrap();
+            prover.prove(&challenge, address, nonce).unwrap();
         });
     })
     .unwrap();
@@ -51,11 +51,11 @@ pub fn prove(pk: Box<ProvingKeyInst>, degree: u32, min_elapse: u64) {
 // }
 
 pub fn bench(degree: u32) {
-    let (provekey, degree) = setup_provekey(degree);
+    let (prover, degree) = setup_prover(degree);
 
     print_backgroud_metrics(6 * 60);
 
-    let pk: Box<ProvingKeyInst> = Box::new(provekey);
-    prove(pk.clone(), degree, 1);
-    prove(pk.clone(), degree, 5);
+    let p: Box<CoinbasePuzzleInst> = Box::new(prover);
+    prove_by_degree(p.clone(), degree, 1);
+    prove_by_degree(p.clone(), degree, 5);
 }
